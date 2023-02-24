c191abc9c194ed26bc5e2391d14403d966438301
AmosPark <pykite2525@gmail.com>
2023-02-24 15:37:48 +0900

    add buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CalculInput from './CalculInput';
+import CalButtons from './CalButtons';
 
 function App() {
   return (
     <>
       <CalculInput />
+      <CalButtons />
     </>
   );
 }
